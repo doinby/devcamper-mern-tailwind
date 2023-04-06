e221cc2ffed277cd17813c85f9262b4956e7eaad
Chinh Do <chinh@doinby.co>
2023-04-06 17:17:34 +0700

    Add Mongoose, connect to MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Import route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to MonogoDB
+connectDB();
+
+// Import route files
+const bootcamps = require('./routes/bootcamps');
 
 // Express App
 const app = express();
@@ -21,7 +25,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 // App Listening Setup
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.MODE_ENV} on port ${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`);
+
+	// Close server and exit process
+	server.close(() => process.exit(1));
+});
