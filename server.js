09446dc3ca46d77d63379660e7aa244993c3ab94
Chinh Do <chinh@doinby.co>
2023-04-06 11:34:11 +0700

    Add test routes and responses

--- server.js
+++ server.js
@@ -7,8 +7,28 @@ dotenv.config({ path: './config/config.env' });
 // Express App
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.get('/api/v1/bootcamp', (req, res) =>
+	res.status(200).json({ success: true, msg: 'Show All Bootcamps' })
+);
 
+app.post('/api/v1/bootcamp', (req, res) =>
+	res.status(200).json({ success: true, msg: 'Create New Bootcamp' })
+);
+
+app.put('api/v1/bootcamp/:id', (req, res) =>
+	res
+		.status(200)
+		.json({ success: true, msg: `Update Bootcamp ${req.params.id}` })
+);
+
+app.delete('api/v1/bootcamp/:id', (req, res) =>
+	res
+		.status(200)
+		.json({ success: true, msg: `Delete Bootcamp ${req.params.id}` })
+);
+
+// App Listening Setup
+const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.MODE_ENV} on port ${PORT}`)
