d813a3afe2938f802b28bc9e0e39af05153c56f3
Chinh Do <chinh@doinby.co>
2023-04-06 16:31:02 +0700

    Add morgan middlewares

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 // Import route files
 const bootcamps = require('./routes/bootcamps');
@@ -10,8 +10,6 @@ dotenv.config({ path: './config/config.env' });
 
 // Express App
 const app = express();
-
-app.use(logger);
 
 // Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
