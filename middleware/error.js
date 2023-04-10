8655c2d2451cc2f6951fcb65076e6e0952a30788
Chinh Do <chinh@doinby.co>
2023-04-10 17:49:58 +0700

    Refactor Mongoose object ID not found

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,20 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
+	let error = { ...err };
+	error.message = err.message;
+
 	console.log(err.stack);
 
+	// Mongoose bad ObjectId
+	if (err.name === 'CastError') {
+		const message = `Resource not found`;
+		error = new ErrorResponse(message, 404);
+	}
+
 	res
-		.status(err.statusCode || 500)
-		.json({ success: false, error: err.message || 'Server Error' });
+		.status(error.statusCode || 500)
+		.json({ success: false, error: error.message || 'Server Error' });
 };
 
 module.exports = errorHandler;
